bc205d8492932cd85fc29b363e4205e5ff100a50
zhanglin <2819207319@qq.com>
2025-01-17 01:46:27 +0800

    consig修改

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    allowedHosts: ['bilibili.hardews.cn'],
+    allowedHosts: 'all',
     proxy:{
         "/":{
             target:'https://video.hardews.cn',//跨域请求的公共地址

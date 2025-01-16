b0ee394154c3dea94dd162231d408f758514e7c5
zhanglin <2819207319@qq.com>
2025-01-17 01:34:31 +0800

    vue.config.js修改

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     proxy:{
+        allowedHosts: ['bilibili.hardews.cn'],
         "/":{
             target:'https://video.hardews.cn',//跨域请求的公共地址
             ws:false, //也可以忽略不写，不写不会影响跨域

ba5303cf0110fcc51adebcd204cfc91dc39e41a3
zhanglin <2819207319@qq.com>
2025-01-17 02:45:32 +0800

    consig修改

--- vue.config.js
+++ vue.config.js
@@ -7,8 +7,12 @@ module.exports = defineConfig({
         "/":{
             target:'https://video.hardews.cn',//跨域请求的公共地址
             secure: false,
+            ws:false,
             changeOrigin:true,
         }
-    }
+    },
+    client: {
+        webSocketURL: 'wss://localhost:8080/ws',
+    },
 },
 })

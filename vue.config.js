521ee9a6644c7acaa545efbd8aed02d27d9705e4
zhanglin <2819207319@qq.com>
2025-01-17 01:51:58 +0800

    consig修改

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,6 @@ module.exports = defineConfig({
     proxy:{
         "/":{
             target:'https://video.hardews.cn',//跨域请求的公共地址
-            ws:false, //也可以忽略不写，不写不会影响跨域
             secure: false,
             changeOrigin:true,
         }
